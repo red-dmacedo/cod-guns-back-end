fd6278ba9613720f73600577cf39213ba27f3d7a
Dae Dowdell <dow_dell2010@yahoo.com>
2025-08-20 16:55:49 -0400

    create route

--- controllers/guns.js
+++ controllers/guns.js
@@ -11,7 +11,20 @@ router.post('/', async (req, res) => { // Create a gun
 });
 
 router.put('/:gunId', async (req, res) => { // Update specific gun
-
+    try {
+    const updatedGun = await Gun.findByIdAndUpdate(req.params.GunId, req.body, { new: true });
+    if (!updatedGun) {
+      res.status(404);
+      throw new Error('Gun not found.');
+    }
+    res.status(200).json(updatedGun);
+  } catch (err) {
+    if (res.statusCode === 404) {
+      res.json({ err: err.message });
+    } else {
+      res.status(500).json({ err: err.message });
+    }
+  }
 });
 
 router.get('/:gunId', async (req, res) => { // Get specific gun
