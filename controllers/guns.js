b6e4ac5a83fd7835d1145337399ee9c61c5b97fe
devin <devinm3333@outlook.com>
2025-08-20 13:53:28 -0700

    Get Gun COMPLETE

--- controllers/guns.js
+++ controllers/guns.js
@@ -15,7 +15,13 @@ router.put('/:gunId', async (req, res) => { // Update specific gun
 });
 
 router.get('/:gunId', async (req, res) => { // Get specific gun
-
+  try {
+    const gun = await Gun.findById(req.params.id);
+    if (!gun) { throw new Error("Gun not found") };
+    return res.status(200).json(gun);
+  } catch (err) {
+    return res.status(400).json({ err: err.message });
+  };
 });
 
 router.delete('/:gunId', async (req, res) => { // Delete specific gun
