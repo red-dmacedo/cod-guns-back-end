2e3f0e6041dde7a9d9ac4c091b7eaef2ce658155
Sharon Lee <ssmlee10@gmail.com>
2025-08-21 07:20:14 -0700

    format guns.js, gun.js, server.js

--- models/gun.js
+++ models/gun.js
@@ -1,11 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const gunSchema = mongoose.Schema({
-  model: {type: String, required: true},
-  category: {type: String, required: true, enum: ['handgun','shotgun','rifle','semi-automatic', 'automatic', 'melee']},
-  ammoType: {type: String, required: true, enum: ['9mm', '50cal', 'hollowPoint', 'slugs', '12ga']},
+  model: { type: String, required: true },
+  category: {
+    type: String,
+    required: true,
+    enum: [
+      "handgun",
+      "shotgun",
+      "rifle",
+      "semi-automatic",
+      "automatic",
+      "melee",
+    ],
+  },
+  ammoType: {
+    type: String,
+    required: true,
+    enum: ["9mm", "50cal", "hollowPoint", "slugs", "12ga"],
+  },
 });
 
-const Gun = mongoose.model('Gun', gunSchema);
+const Gun = mongoose.model("Gun", gunSchema);
 
 module.exports = Gun;
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // ===== Imports =====
-const dotenv = require('dotenv');
-const express = require('express');
-const methodOverride = require('method-override');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const cors = require('cors');
-const gunRouter = require('./controllers/guns');
+const dotenv = require("dotenv");
+const express = require("express");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+const gunRouter = require("./controllers/guns");
 
 // ===== Constant Vars =====
 dotenv.config();
@@ -16,17 +16,17 @@ const PORT = process.env.PORT ? process.env.PORT : 3000;
 // ===== Hooks =====
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on('connected', () => {
+mongoose.connection.on("connected", () => {
   console.log(`Conncted to MongoDB ${mongoose.connection.name}`);
 });
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
-app.use(methodOverride('_method'));
+app.use(morgan("dev"));
+app.use(methodOverride("_method"));
 
 // ===== Routes =====
-app.use('/guns', gunRouter);
+app.use("/guns", gunRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
